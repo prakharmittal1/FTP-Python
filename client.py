c037bf6ebb900b27a0a85023feddaaeade0e12d2
Prakhar Mittal <37546569+prakharmittal1@users.noreply.github.com>
2019-10-11 18:35:47 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -12,30 +12,33 @@ print("\nConnected to Server Successfully !  ")
 
 
 while True:
-	print("\nChoose Option :   \n")
-	selector=int(input("1. List the files on the Server \n2. Upload a file to the server \n3. Download a file from the server \n4. Quit \n\nEnter your choice : "))
-	if selector==4:
+	print("\nChoose Option :  ")
+	selector=str(input("dir \nupload\ndownload \nquit \n\n>> "))
+	x=selector.split(" ")
+	if selector=='quit':
 		break
 		exit()
 
-	elif selector==1:
+	elif selector=='dir':
 		data = []
 		ftp.dir(data.append)
 		for line in data:
 			print("-", line)
-		print("List of Files displayed successfully. ")
+		print("END OF LIST ")
 
-	elif selector==2:
-		filenameup=input("Enter the Name of the file you want to Upload : ")
+	elif x[0]=='upload':
+		filenameup=x[1]
+		#input("Enter the Name of the file you want to Upload : ")
 		def upload(ftp, filenameup):
 			ext = os.path.splitext(filenameup)[1]
 			ftp.storbinary("STOR " + filenameup, open(filenameup, "rb"), 1024)
 		upload(ftp, filenameup)
-		print(filenameup," : Uploaded successfully... ")
+		print(filenameup," : Uploaded successfully. ")
 
 
-	elif selector==3:
-		filename=input("Enter the Name of the file you want to Download : ")
+	elif x[0]=='download':
+		filename = x[1]
+		#filename=input("Enter the Name of the file you want to Download : ")
 		def getFile(ftp, filename):
 			try:
 				ftp.retrbinary("RETR " + filename ,open(filename, 'wb').write)
