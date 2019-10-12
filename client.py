08e8d60970d6eeb56f1d30f89ead014597234b84
Prakhar Mittal <37546569+prakharmittal1@users.noreply.github.com>
2019-10-12 16:42:10 -0400

    Add files via upload

--- client.py
+++ client.py
@@ -3,55 +3,68 @@ import sys
 import os
 import getpass
 
-print("\nEnter Server Details:          \n")
-ftp = ftplib.FTP(input("Enter IP:"))
-Username=input("Username: ")
-Password=getpass.getpass()
-ftp.login(Username, Password)
-print("\nConnected to Server Successfully !  ")
-
 
 while True:
-	print("\nChoose Option :  ")
-	selector=str(input("dir \nupload\ndownload \nquit \n\n>> "))
-	x=selector.split(" ")
-	if selector=='quit':
-		break
-		exit()
 
-	elif selector=='dir':
-		data = []
-		ftp.dir(data.append)
-		for line in data:
-			print("-", line)
-		print("END OF LIST ")
+	print("\nEnter Server Details:          \n")
 
-	elif x[0]=='upload':
-		filenameup=x[1]
-		#input("Enter the Name of the file you want to Upload : ")
-		def upload(ftp, filenameup):
-			ext = os.path.splitext(filenameup)[1]
-			ftp.storbinary("STOR " + filenameup, open(filenameup, "rb"), 1024)
-		upload(ftp, filenameup)
-		print(filenameup," : Uploaded successfully. ")
+	option=str(input())
+	y=option.split(" ")
 
+	if option=='quit':
+			break
+			exit()
 
-	elif x[0]=='download':
-		filename = x[1]
-		#filename=input("Enter the Name of the file you want to Download : ")
-		def getFile(ftp, filename):
-			try:
-				ftp.retrbinary("RETR " + filename ,open(filename, 'wb').write)
-			except:
-				print("Error")
-		ftp.cwd('')
-		getFile(ftp,filename)
-		print(filename," : Downloaded successfully... ")
+	elif y[0]=='ftpclient':
+		ftpIP=y[1]
+		#ftp = ftplib.FTP(input(ftpIP))
+		ftp = ftplib.FTP()
+		ftp.connect(ftpIP,21)
+		#ftp=y[1]
+		Username=input("Username: ")
+		Password=getpass.getpass()
+		ftp.login(Username, Password)
+		print("\nConnected to Server Successfully ! \n ")
 
+	while True:
 
-	else:
-		print("Invalid Choice, Please choose again")
-		continue
+		selector=str(input())
+		x=selector.split(" ")
+		if selector=='quit':
+			break
+			exit()
 
+		elif selector=='dir':
+			data = []
+			ftp.dir(data.append)
+			for line in data:
+				print(line)
+			print("END OF LIST \n")
+
+		elif x[0]=='upload':
+			filenameup=x[1]
+			def upload(ftp, filenameup):
+				ext = os.path.splitext(filenameup)[1]
+				ftp.storbinary("STOR " + filenameup, open(filenameup, "rb"), 1024)
+			upload(ftp, filenameup)
+			print(filenameup," : Uploaded successfully. \n")
+
+		elif x[0]=='get':
+			filename = x[1]
+			def getFile(ftp, filename):
+				try:
+					ftp.retrbinary("RETR " + filename ,open(filename, 'wb').write)
+				except:
+					print("Error")
+					return
+			ftp.cwd('')
+			getFile(ftp,filename)
+			print(filename," : Downloaded successfully. \n ")
+
+		else:
+			print("Invalid Command, Please choose again \n")
+			continue
+
+	ftp.quit()		
 
 ftp.quit()
--- server.py
+++ server.py
@@ -2,13 +2,12 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import FTPServer
 
-
 authorizer = DummyAuthorizer()
 
-authorizer.add_user("prakhar", "prakhar", "D:\Demo", perm="elradfmwMT")
-authorizer.add_user("mittal", "mittal", "D:\Demo", perm="elradfmwMT")
+authorizer.add_user("prakhar", "prakhar", "D:\Demo\Client Files", perm="elradfmwMT")
+authorizer.add_user("mittal", "mittal", "D:\Demo\Client Files", perm="elradfmwMT")
 
-authorizer.add_anonymous("D:\Demo") #path of the directory you want to the clients to access
+authorizer.add_anonymous("D:\Demo\Server Files") #path of the directory you want to the clients to access
 handler = FTPHandler
 handler.authorizer = authorizer
 server = FTPServer(("172.16.105.58", 21), handler) #the IP address of your system, the one which is the server
